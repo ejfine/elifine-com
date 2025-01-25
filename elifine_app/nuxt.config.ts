8c2ba84f9b2b3813638b17db81f4478c7123ab0d
Eli Fine <ejfine@gmail.com>
2025-01-25 19:04:03 +0000

    MVP website

--- elifine_app/nuxt.config.ts
+++ elifine_app/nuxt.config.ts
@@ -1,5 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+
+  // https://github.com/nuxt/nuxt/issues/30461#issuecomment-2572616714
+  experimental: { appManifest: false },
+
+  vite: { // this seems to be explicitly needed when in a devcontainer in order for hot reloading to work
+    server: {
+      watch: {
+        usePolling: true,
+      },
+    },
+  },
+
+  modules: ['@nuxt/ui'],
 })
