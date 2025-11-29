f57392ef7ef7b8d0d664b76ae115c695ef69766d
Eli Fine <eli88fine@gmail.com>
2025-11-29 01:14:17 +0000

    glo

--- elifine_app/global.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference types="nuxt/app" />
-// needed to help avoid typescript error in app.config.ts
-declare global {
-  const defineAppConfig: typeof import("nuxt/app").defineAppConfig;
-}
-
-export {};
